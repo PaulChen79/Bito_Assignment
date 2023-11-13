d038ddab617bd5e5c7f4fc60a23a8db2d2099f05
pinhchen <pinhchen@amd.com>
2023-11-13 12:01:29 +0800

    feat: 🎸 add domain error format and api helper

---
+++ domain/error.go
@@ -0,0 +1,19 @@
+package domain
+
+type ErrorFormat struct {
+	Code    int
+	Message string
+}
+
+type ResponseFormat struct {
+	Code int         `json:"code" example:"200"`
+	Data interface{} `json:"data"`
+}
+
+var (
+	ErrorUnauthorized        = ErrorFormat{Code: 401, Message: "Unauthorized"}
+	ErrorForbidden           = ErrorFormat{Code: 403, Message: "Forbidden"}
+	ErrorBadRequest          = ErrorFormat{Code: 400, Message: "bad request"}
+	ErrorServer              = ErrorFormat{Code: 500, Message: "Server Error"}
+	ErrorUnknowInternalError = ErrorFormat{Code: 404, Message: "Unknow Internal Error"}
+)
